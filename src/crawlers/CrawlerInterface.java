0f96513b379554eb2559c4a12c3e8e3c5579d43a
Daniil Zinenko <me@bobrnor.com>
2013-10-12 12:08:05 +0600

    create project

---
+++ src/crawlers/CrawlerInterface.java
@@ -0,0 +1,11 @@
+package crawlers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bobrnor
+ * Date: 10/12/13
+ * Time: 12:03
+ * To change this template use File | Settings | File Templates.
+ */
+public interface CrawlerInterface {
+}
